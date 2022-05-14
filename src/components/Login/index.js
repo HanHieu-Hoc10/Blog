cd291d701a11ef46401b71f3bffc3f3442ca19e8
HanHieu-Hoc10 <hieu.han@hoc10.com>
2022-05-14 23:55:07 +0700

    update logic login register

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Form } from "semantic-ui-react";
 import axios from "axios";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Register from "../Register";
-// import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  //   const history = useHistory();
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [checkbox, setCheckbox] = useState(false);
+  let navigate = useNavigate();
+  const [userName, setuserName] = useState("");
+  const [passWord, setPassWord] = useState("");
 
   const [APIData, setAPIData] = useState([]);
   useEffect(() => {
@@ -20,71 +19,48 @@ export default function Login() {
         setAPIData(response.data);
       });
   }, []);
-
   const setData = () => {
-    // let { id, firstName, lastName, checkbox } = data;
-    APIData.map((item, index) => {
-      if (item?.userName == firstName && item?.passWord == lastName) {
-        // history.push("/register");
-        localStorage.setItem("First Name", firstName);
-        localStorage.setItem("Last Name", lastName);
-        localStorage.setItem("Checkbox Value", checkbox);
-      } else if (item?.userName != firstName || item?.passWord != lastName) {
-        // alert("TK hoặc Mk không chính xác");
+    APIData.map((item) => {
+      if (item?.userName == userName && item?.passWord == passWord) {
+        localStorage.setItem("userName", userName);
+        localStorage.setItem("passWord", passWord);
+        toast("Đăng nhập thành công");
+        navigate("blog-detail");
+      } else if (item?.userName != userName || item?.passWord != passWord) {
+        toast("Tài khoản hoặc mk không chính xác");
       }
-
-      console.log(item);
     });
-    // // useEffect(() => {
-    // //   setAPIData();
-    // // }, []);
-    // console.log("APIData", APIData);
   };
-  useEffect(() => {
-    setData();
-  }, [APIData]);
+
+  const submit = () => {};
   return (
     <div>
+      <ToastContainer />
       <Form className="create-form ml-5 mt-5">
         <h1> Đăng nhập</h1>
         <Form.Field>
           <label>user name</label>
           <input
             placeholder="First Name"
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={(e) => setuserName(e.target.value)}
           />
         </Form.Field>
         <Form.Field>
           <label>Pass Word</label>
           <input
             placeholder="Last Name"
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={(e) => setPassWord(e.target.value)}
           />
         </Form.Field>
-        <Button onClick={setData} type="submit">
+        <Button onClick={() => setData()} type="submit">
           Submit
         </Button>
         <button type="submit">
           <Link to={"/dang-ky"} style={{ color: "#fff" }}>
-            {" "}
-            Đăng ký{" "}
+            Đăng ký
           </Link>
         </button>
       </Form>
-
-      {/* <Routes>
-        <Route path="/" element={<Register />}>
-          {/* <Route index element={<CommentBlog />} />
-              <Route path="dang-ky" element={<Register />}>
-                <Route path=":dang-ky" element={<Register />} />
-                <Route path="new" element={<BlogDetail />} />
-
-                <Route path="dang-tin" element={<PostBlog />} />
-                <Route path=":dang-tin" element={<PostBlog />} />
-                <Route index element={<CommentBlog />} />
-              </Route> */}
-      {/* </Route> */}
-      {/* </Routes> */}
     </div>
   );
 }
